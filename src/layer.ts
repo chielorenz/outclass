7339373172f6d6106b64a8a28bbd392d1ce9e664
b1n01 <chielorenz@gmail.com>
2023-06-10 22:45:43 +0200

    Remove Stype namespace; now layer.parse() accepts an ActionMap

--- src/layer.ts
+++ src/layer.ts
@@ -1,31 +1,34 @@
-namespace Stype {
-  export type Value = string | undefined | null | boolean;
+export type Value = string | undefined | null | boolean;
 
-  export type Object = {
-    [key: string]: Value | List | Object;
-  };
+export type Object = {
+  [key: string]: Value | List | Object;
+};
 
-  export type Item = Value | List | Object;
+export type Item = Value | List | Object;
 
-  export type List = Array<Item>;
+export type List = Array<Item>;
 
-  export type ActionTypes = "add" | "remove" | "set" | string;
+export type ActionType = "add" | "remove" | "set";
 
-  export type Action = {
-    type: ActionTypes;
-    tokens: Set<string>;
-  };
+export type Action = {
+  type: ActionType;
+  tokens: Set<string>;
+};
 
-  export type ActionMap = { [key in ActionTypes]: List };
+export type ActionMap = {
+  add: List;
+  remove: List;
+  set: List;
+  patch: Patch;
+};
 
-  export type Patch = Action[];
-}
+export type Patch = Action[];
 
-function isArray(item: unknown): item is Stype.List {
+function isList(item: unknown): item is List {
   return Object.prototype.toString.call(item) === "[object Array]";
 }
 
-function isObject(item: unknown): item is Stype.Object {
+function isObject(item: unknown): item is Object {
   return Object.prototype.toString.call(item) === "[object Object]";
 }
 
@@ -33,11 +36,11 @@ function isString(item: unknown): item is string {
   return Object.prototype.toString.call(item) === "[object String]";
 }
 
-function parse(...params: Stype.List): Set<string> {
+function parse(...params: List): Set<string> {
   const tokens = new Set<string>();
 
-  function eat(param: Stype.Item): Set<string> {
-    if (isArray(param)) {
+  function eat(param: Item): Set<string> {
+    if (isList(param)) {
       return parse(...param);
     } else if (isObject(param)) {
       return parse(...Object.values(param));
@@ -58,7 +61,7 @@ function parse(...params: Stype.List): Set<string> {
 }
 
 class Parser {
-  public parse(...params: Stype.List): string {
+  public parse(...params: List): string {
     return [...parse(params)].join(" ");
   }
 
@@ -68,47 +71,54 @@ class Parser {
 }
 
 class Layer {
-  private actions: Stype.Action[] = [];
-  private patches: Stype.Patch[] = [];
+  private actions: Action[] = [];
+  private patches: Patch[] = [];
 
-  public add(...params: Stype.List): Layer {
+  public add(...params: List): Layer {
     this.actions.push({ type: "add", tokens: parse(params) });
     return this;
   }
 
-  public remove(...params: Stype.List): Layer {
+  public remove(...params: List): Layer {
     this.actions.push({ type: "remove", tokens: parse(params) });
     return this;
   }
 
-  public set(...params: Stype.List): Layer {
+  public set(...params: List): Layer {
     this.actions.push({ type: "set", tokens: parse(params) });
     return this;
   }
 
-  public with(actions: Stype.ActionMap): Layer {
-    for (const type in actions) {
-      const tokens = parse(actions[type]);
-      this.actions.push({ type, tokens });
-    }
-    return this;
-  }
-
-  public apply(...patches: Stype.Patch[]): Layer {
+  public apply(...patches: Patch[]): Layer {
     for (const patch of patches) {
       this.patches.push(patch);
     }
     return this;
   }
 
-  get patch(): Stype.Patch {
+  public with(actions: ActionMap): Layer {
+    let type: keyof ActionMap;
+    for (type in actions) {
+      if (type === "patch") {
+        this.patches.push(actions[type]);
+      } else {
+        const tokens = parse(actions[type]);
+        this.actions.push({ type, tokens });
+      }
+    }
+    return this;
+  }
+
+  get patch(): Patch {
     return this.actions;
   }
 
-  public parse(): string {
+  public parse(actions: ActionMap): string {
+    this.with(actions);
+
     let tokens = new Set<string>();
 
-    function eat(action: Stype.Action) {
+    function eat(action: Action) {
       if (action.type === "add") {
         action.tokens.forEach((token) => tokens.add(token));
       } else if (action.type === "remove") {
