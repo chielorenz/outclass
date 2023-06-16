7f947fb744563b67ee245d9b4959977201b715b8
b1n01 <chielorenz@gmail.com>
2023-06-17 00:03:45 +0200

    Update tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,61 +1,67 @@
-import { s } from "../src/index";
+import { out } from "../src/index";
 
 describe("Parser", () => {
   test("Empty", () => {
-    expect(s.parse()).toBe("");
-    expect(s.parse(null)).toBe("");
-    expect(s.parse(undefined)).toBe("");
-    expect(s.parse("")).toBe("");
-    expect(s.parse([])).toBe("");
-    expect(s.parse(true)).toBe("");
-    expect(s.parse(false)).toBe("");
+    expect(out.parse()).toBe("");
+    expect(out.parse(null)).toBe("");
+    expect(out.parse(undefined)).toBe("");
+    expect(out.parse("")).toBe("");
+    expect(out.parse([])).toBe("");
+    expect(out.parse(true)).toBe("");
+    expect(out.parse(false)).toBe("");
   });
 
   test("String", () => {
-    expect(s.parse("a")).toBe("a");
-    expect(s.parse("a b")).toBe("a b");
+    expect(out.parse("a")).toBe("a");
+    expect(out.parse("a b")).toBe("a b");
   });
 
   test("Array", () => {
-    expect(s.parse(["a"])).toBe("a");
-    expect(s.parse(["a", "b"])).toBe("a b");
+    expect(out.parse(["a"])).toBe("a");
+    expect(out.parse(["a", "b"])).toBe("a b");
   });
 
   test("Trim", () => {
-    expect(s.parse(" a ")).toBe("a");
-    expect(s.parse(" a  b ")).toBe("a b");
+    expect(out.parse(" a ")).toBe("a");
+    expect(out.parse(" a  b ")).toBe("a b");
   });
 
   test("Sort", () => {
-    expect(s.parse("a b")).toBe("a b");
-    expect(s.parse("b a")).toBe("b a");
+    expect(out.parse("a b")).toBe("a b");
+    expect(out.parse("b a")).toBe("b a");
 
-    expect(s.parse(["a", "b"])).toBe("a b");
-    expect(s.parse(["b", "a"])).toBe("b a");
+    expect(out.parse(["a", "b"])).toBe("a b");
+    expect(out.parse(["b", "a"])).toBe("b a");
   });
 
   test("Multiple", () => {
-    expect(s.parse("a", "b")).toBe("a b");
-    expect(s.parse(["a"], ["b"])).toBe("a b");
+    expect(out.parse("a", "b")).toBe("a b");
+    expect(out.parse(["a"], ["b"])).toBe("a b");
   });
 
   test("Nesting", () => {
-    expect(s.parse([["a"]])).toBe("a");
-    expect(s.parse([["a"], ["b"]])).toBe("a b");
+    expect(out.parse([["a"]])).toBe("a");
+    expect(out.parse([["a"], ["b"]])).toBe("a b");
   });
 
   test("Repeated", () => {
-    expect(s.parse("a a")).toBe("a");
-    expect(s.parse("a", "a")).toBe("a");
-    expect(s.parse("a a", "a a")).toBe("a");
+    expect(out.parse("a a")).toBe("a");
+    expect(out.parse("a", "a")).toBe("a");
+    expect(out.parse("a a", "a a")).toBe("a");
   });
 
   test("Performance", () => {
     const length = 100000;
     const big = Array.from({ length }, () => "a");
-    s.parse(big);
+    out.parse(big);
     const usedBytes = process.memoryUsage().heapUsed / 1024 / 1024;
     const usedMB = Math.round(usedBytes * 100) / 100;
     expect(usedMB).toBeLessThan(200);
   });
 });
+
+// describe("Slot", () => {
+//   test("Test", () => {
+//     expect(out.slot.set("a", "a", "b").parse()).toBe("a b");
+//   });
+// });
