e99d65ca356914cbcde221f82725ed8f23dd39a3
Luca Lorenzini <chielorenz@gmail.com>
2026-05-22 18:54:17 +0200

    chore

--- repl.ts
+++ repl.ts
@@ -1,8 +1,6 @@
-import repl from 'node:repl';
-import { out } from './src/index';
+import repl from "node:repl";
+import { out } from "./src/index";
 
 const replServer = repl.start();
 replServer.context.out = out;
 replServer.write('out.parse("outclass"); // out object is available\n');
-
-
